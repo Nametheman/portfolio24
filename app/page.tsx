82e7084ad9c1c2447746751f51e11d70990bff6d
Emmanuel Oluwasegun Owolabi <owoemma59@gmail.com>
2024-09-21 11:15:09 +0100

    f

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import PastProjects from "@/components/PastProjects";
 export default function Home() {
   const [showLoader, setLoader] = useState(true);
   const [showReveal, setShowReveal] = useState(false);
+  //test
   useEffect(() => {
     const timeout = 3000;
     const loaderTimeout = setTimeout(() => {
